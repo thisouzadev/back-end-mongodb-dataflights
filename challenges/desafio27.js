297db9fb9da08ad64bb7aea88453f7e45db759ca
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-18 15:13:31 -0300

    desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({
+  natureza: "Doméstica",
+  "empresa.nome": "PASSAREDO",
+  }).count(),
+});
+db.resumoVoos.find({
+  empresa: "PASSAREDO",
+}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
